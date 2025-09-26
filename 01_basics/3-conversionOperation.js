7e4792a5912a93fa7b115b8fc9fb531a3fb644d1
Chinmayyy03 <chinmayghewade7755@gmail.com>
2025-09-26 07:15:54 +0000

    conversion into datatypes

---
+++ 01_basics/3-conversionOperation.js
@@ -0,0 +1,20 @@
+let score = "undefined"
+
+// console.log(typeof score);
+
+let valueIntNumber = Number(score)
+// console.log(typeof valueIntNumber);
+// console.log(valueIntNumber);
+
+let name = 00
+let intoBoolean = Boolean(name)
+// console.log(intoBoolean);
+
+
+let Stringnumber = 11
+let intoString = String(Stringnumber)
+console.log(intoString);
+console.log(typeof intoString);
+
+
+
