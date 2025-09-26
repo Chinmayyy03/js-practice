70230f0a3e5bdc421c59196a4f2cfb5767d50dd8
Chinmayyy03 <chinmayghewade7755@gmail.com>
2025-09-26 06:26:58 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,13 @@
+let id = 123
+let name = "chinmay"
+let email = null
+let mobile = undefined
+let value = true
+
+// console.table([id,name,email,mobile,value])
+
+console.log(typeof id)
+console.log(typeof name)
+console.log(typeof email)
+console.log(typeof mobile)
+console.log(typeof value);
