e885f53a0c706403362ad0d9d9e0361f8926063d
Chinmayyy03 <chinmayghewade7755@gmail.com>
2025-09-27 07:20:14 +0000

    added date

---
+++ 01_basics/6-datesInJs.js
@@ -0,0 +1,19 @@
+// Dates
+
+let mydate = new Date()
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toLocaleString());
+// console.log(typeof mydate);
+
+
+// let myCreatedDate = new Date(2023,1,23)
+// let myCreatedDate = new Date(2023,0,23,5,3,9)
+let myCreatedDate = new Date("2023-01-14")
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+
+  console.log(mydate.getHours());
+  
