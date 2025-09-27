664d0cb107ac49b24c016f09fd22003d25b76cbe
Chinmayyy03 <chinmayghewade7755@gmail.com>
2025-09-27 06:19:06 +0000

    num and math added

---
+++ 01_basics/5-numsAndMath.js
@@ -0,0 +1,38 @@
+const score = 400
+// console.log(score);
+
+
+const balance = new Number (100)
+// console.log(balance);
+
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const otherNumber = 2234599
+
+// console.log(otherNumber.toPrecision(4));
+// console.log(otherNumber.toLocaleString('en-IN'));
+
+// ++++++++++++++++Math++++++++++++++
+
+// console.log(Math.abs(-3));
+// console.log(Math.round(3.4));
+// console.log(Math.ceil(3.4));
+// console.log(Math.floor(3.4));
+// console.log(Math.min(3.4,3.3));
+// console.log(Math.max(3.4,3.3));
+
+console.log(Math.random());
+console.log(Math.random()*10);
+console.log(Math.floor(Math.random()*10));
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max-min+1)+min));
+
+
+
+
+
