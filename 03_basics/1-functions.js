95870b05402c212bca8d7accc94ce795dd56f5a9
Chinmayyy03 <chinmayghewade7755@gmail.com>
2025-09-29 08:22:49 +0000

    functions

---
+++ 03_basics/1-functions.js
@@ -0,0 +1,40 @@
+
+function sayMyName(){
+console.log("C");
+console.log("h");
+console.log("i");
+console.log("n");
+console.log("m");
+console.log("a");
+console.log("y");
+}
+
+// sayMyName
+
+// function addTwoNumbers(number1,number2){
+//     console.log(number1 + number2);
+// }
+
+
+function addTwoNumbers(number1,number2){
+    // let result= number1 + number2
+    // return result
+
+    return number1+number2
+}
+
+const result = addTwoNumbers(3,5)
+// console.log("Result:", result);
+
+
+function loginUserMessage(username = "sam"){
+    if (!username) {
+    console.log("Please enter a username");
+    return     
+    }
+    return `${username} just logged in`
+}
+// console.log(loginUserMessage("chinmay"))
+console.log(loginUserMessage("Chinmay"))
+
+
