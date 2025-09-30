0d35b444c422ed9610a6c7f899fa31c9af38ba43
Chinmayyy03 <chinmayghewade7755@gmail.com>
2025-09-30 11:27:04 +0000

    change

---
+++ 05_iterations/one.js
@@ -0,0 +1,50 @@
+
+
+// for (let index = 0; index <= 10; index++) {
+//     const element = index;
+//     if (index==5) {
+//         console.log("5 is best number");
+//     }
+//     console.log(index);
+// }
+
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`table of ${i}`);
+//     for (let j = 1; j <= 10; j++) {
+//         // console.log(`inner loop ${j} and inner loop ${i}`);
+//         console.log(i + '*' + j + '=' + i*j);
+//     }
+// }
+
+// let myArr = ['batman','superman','flash']
+// console.log(myArr.length);
+
+// for (let i = 0; i < myArr.length; i++) {
+//     const element = myArr[i];
+//     console.log(element);
+// }
+
+
+// for (let i = 0; i <=20; i++) {
+//     if (i==5) {
+//         console.log('detected 5');
+//         break      
+//     }
+//     console.log(`value of i is ${i}`);
+// }
+
+
+for (let i = 0; i <=20; i++) {
+    if (i==5) {
+        console.log('detected 5');
+        continue      
+    }
+    console.log(`value of i is ${i}`);
+    
+}
+
+
+
+
+
