14bd859951df8a7ecbb142c2b431b2c5d2c14d5d
Chinmayyy03 <chinmayghewade7755@gmail.com>
2025-10-01 06:09:15 +0000

    two added

---
+++ 05_iterations/two.js
@@ -0,0 +1,36 @@
+
+// let index=1
+// while (index<=10) {
+//     console.log(`value is ${index}`);
+//     index=index+1
+// }
+
+
+// let myArr = ["chinmay","aditya","yashvardhan"]
+// let arr = 0
+// while (arr < myArr.length) {
+//     console.log(`value is ${myArr[arr]}`);
+//     arr = arr + 1
+// }
+
+// let myArr = ["chinmay","aditya","yash"]
+// for (let index = 0; index < myArr.length; index++) {
+//     const element = myArr[index];
+//     console.log(element);
+// }
+
+
+let index = 0
+do {
+    console.log(`value is ${index}`);
+    index ++
+    
+} while (index <= 10);
+
+
+// let myArr = ["chinmay","aditya","yash"]
+// let index = 0
+// do {
+//     console.log("value is " + myArr[index]);
+//     index++
+// } while (index < myArr.length);
